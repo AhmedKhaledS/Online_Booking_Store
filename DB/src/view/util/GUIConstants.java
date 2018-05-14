3c145c0b60c84ab2dc387784548acfe6e52a863d
Ahmed El-Naggar <ahmednagar2011@gmail.com>
2018-05-14 18:45:31 +0200

    Enhanced Sign Up frame, and Added Edit Profile Page

---
+++ DB/src/view/util/GUIConstants.java
@@ -0,0 +1,10 @@
+package view.util;
+
+public class GUIConstants {
+    public static final int initY = 50;
+    public static final int initX = 50;
+    public static final int width = 150;
+    public static final int height = 30;
+    public static final int offsetX = 230;
+    public static final int offsetY = 50;
+}
--- DB/src/view/util/WindowChanger.java
+++ DB/src/view/util/WindowChanger.java
@@ -1,7 +1,5 @@
 package view.util;
 
 public interface  WindowChanger {
-    public static void changeWindow () {
-
-    }
+    public static void changeWindow () {}
 }
