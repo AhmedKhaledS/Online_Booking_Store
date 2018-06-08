95f8cf9575cfcbf5a469f703c4baf1ec6e3d992b
Ahmed El-Naggar <ahmednagar2011@gmail.com>
2018-06-08 13:59:53 +0200

    Changed UI Theme to Nimbus

--- DB/src/view/AppMainScreen.java
+++ DB/src/view/AppMainScreen.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.*;
+
 public class AppMainScreen {
     public static void main(String[] a) {
         try {
@@ -7,7 +9,22 @@ public class AppMainScreen {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
         StartUpFrame.changeWindow();
     }
 
